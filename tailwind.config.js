581975439468792b9058c0b333c9c341973a16e7
asimenstad <a-nna-s@hotmail.com>
2022-11-18 15:52:18 +0100

    Add index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./src/**/*.js"],
+  content: ["./*.html", "./src/**/*.{js, mjs}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: "DM Serif Display, serif",
+        body: "Raleway, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
