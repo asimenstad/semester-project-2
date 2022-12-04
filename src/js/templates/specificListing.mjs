a9c9cfb16dd17e09a1b202b4962326b195358b46
asimenstad <a-nna-s@hotmail.com>
2022-12-04 20:10:25 +0100

    Update specific listing template

--- src/js/templates/specificListing.mjs
+++ src/js/templates/specificListing.mjs
@@ -11,11 +11,12 @@ export function specificListingTemplate(data) {
     updated,
   } = data;
 
-  /// Container
+  /// Containers
   const listing = document.createElement("div");
+  const mediaContainer = document.createElement("div");
+  const infoAndBidsContainer = document.createElement("div");
 
   /// Media
-  const mediaContainer = document.createElement("div");
   const mediaImg = document.createElement("img");
 
   mediaImg.src = media[0];
@@ -31,6 +32,8 @@ export function specificListingTemplate(data) {
   const listingTitle = document.createElement("h1");
   const listingDescription = document.createElement("p");
   const bidEndingContainer = document.createElement("div");
+  const bidTitle = document.createElement("h2");
+  const bidFormContainer = document.createElement("div");
 
   const createdFormatted = new Date(created).toLocaleString("en-GB", { timeStyle: "short", dateStyle: "short" });
   const updatedFormatted = new Date(updated).toLocaleString("en-GB", { timeStyle: "short", dateStyle: "short" });
@@ -43,6 +46,7 @@ export function specificListingTemplate(data) {
   }
   listingTitle.textContent = title;
   listingDescription.textContent = description;
+  bidTitle.textContent = "Bid on item";
 
   let highestBid = "None";
   if (bids.length >= 1) {
@@ -52,33 +56,34 @@ export function specificListingTemplate(data) {
   bidEndingContainer.innerHTML = `<div class="flex flex-col"><p class="uppercase">Highest bid</p><p class="font-medium">${highestBid}</p></div>
   <div class="flex flex-col"><p class="uppercase">Ending</p><p class="font-medium">${endsAtFormatted}</p></div>`;
 
-  /// Bids
-  const bidFormContainer = document.createElement("div");
-  const bidsHeader = document.createElement("h2");
-  const bidsContainer = document.createElement("div");
-
-  bidFormContainer.innerHTML = `<form class="my-6">
+  bidFormContainer.innerHTML = `<form class="my-2 flex w-full gap-2">
   <label for="bid" class="sr-only">Bid on listing</label>
   <input type="number"
   name="bid"
   id="bid"
-  class="border-none rounded-sm p-1"
+  class="border-none rounded-sm p-1 w-full"
   required></input>
   <button type="submit"
-  class="btn border border-main bg-main text-white mx-auto px-4 py-1 rounded-sm hover:bg-black hover:text-white hover:border-black">Bid</button>
+  class="btn border border-main w-full bg-main text-white mx-auto px-4 py-1.5 rounded-sm hover:bg-black hover:text-white hover:border-black">Bid</button>
   </form>`;
+
+  /// Bids
+  const bidsContainer = document.createElement("div");
+  const bidsHeader = document.createElement("h2");
+  const allBids = document.createElement("div");
 
   bidsHeader.textContent = `All bids (${bids.length})`;
 
   bids.forEach((bid) => {
     const { amount, bidderName, created } = bid;
     const createdFormatted = new Date(created).toLocaleString("en-GB", { timeStyle: "short", dateStyle: "short" });
-    bidsContainer.innerHTML += `<div class="flex items-center justify-between py-4"><div><p>${bidderName}</p><p>${createdFormatted}</p></div><div><p>${amount} $</p></div></div>`;
+    allBids.innerHTML += `<div class="flex items-center justify-between py-4"><div><p>${bidderName}</p><p>${createdFormatted}</p></div><div><p>${amount} $</p></div></div>`;
   });
 
   /// Classes
-  listing.classList.add("flex", "flex-col", "bg-lightGray", "rounded-sm", "mx-auto", "md:flex-row");
-  infoContainer.classList.add("p-6");
+  listing.classList.add("flex", "flex-col", "rounded-sm", "mx-auto");
+  infoAndBidsContainer.classList.add("flex", "flex-col", "justify-between", "bg-lightGray", "lg:flex-row");
+  infoContainer.classList.add("p-6", "shrink");
   listingHeader.classList.add(
     "flex",
     "flex-wrap",
@@ -89,24 +94,21 @@ export function specificListingTemplate(data) {
     "capitalize"
   );
   listingTitle.classList.add("font-medium", "text-lg", "mt-4");
-  bidEndingContainer.classList.add("flex", "justify-between", "my-4");
+  listingDescription.classList.add("max-w-lg");
+  bidEndingContainer.classList.add("flex", "justify-between", "py-4", "border-b", "border-gray");
+  bidTitle.classList.add("font-medium", "text-lg", "mt-4");
   bidFormContainer.classList.add("flex", "items-center");
+  bidsContainer.classList.add("p-6", "bg-white", "shrink-0", "grow");
   bidsHeader.classList.add("font-medium", "text-lg");
-  bidsContainer.classList.add("divide-y", "divide-gray", "my-4", "capitalize");
+  allBids.classList.add("divide-y", "divide-gray", "my-4", "capitalize");
 
   /// Append
   mediaContainer.append(mediaImg);
   listingHeader.append(listingSeller, listingCreated);
-  infoContainer.append(
-    listingHeader,
-    listingTitle,
-    listingDescription,
-    bidEndingContainer,
-    bidFormContainer,
-    bidsHeader,
-    bidsContainer
-  );
-  listing.append(mediaContainer, infoContainer);
+  infoContainer.append(listingHeader, listingTitle, listingDescription, bidEndingContainer, bidTitle, bidFormContainer);
+  bidsContainer.append(bidsHeader, allBids);
+  infoAndBidsContainer.append(infoContainer, bidsContainer);
+  listing.append(mediaContainer, infoAndBidsContainer);
 
   return listing;
 }
