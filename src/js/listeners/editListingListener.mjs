42dabdd68d86e8045a6b57424ac245c276496ae4
asimenstad <a-nna-s@hotmail.com>
2022-12-14 23:30:06 +0100

    Fixed edit listing form

--- src/js/listeners/editListingListener.mjs
+++ src/js/listeners/editListingListener.mjs
@@ -4,14 +4,65 @@ import { editListing } from "../api/editListing.mjs";
 export function editListingListener() {
   const form = document.getElementById("editListingForm");
   const { titleInput, descriptionInput, tagsInput, mediaInput } = document.getElementById("editListingForm").elements;
+
+  let mediaArray = [];
+  let tagsArray = [];
+
+  mediaArray.push(mediaInput);
+  tagsArray.push(tagsInput);
+
+  const mediaBtn = document.getElementById("mediaBtn");
+  const tagsBtn = document.getElementById("tagsBtn");
+
+  mediaBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const lastMediaElement = mediaArray[mediaArray.length - 1];
+    if (lastMediaElement.value !== "") {
+      const newMediaInput = lastMediaElement.cloneNode(true);
+      newMediaInput.value = "";
+      lastMediaElement.after(newMediaInput);
+      mediaArray.push(newMediaInput);
+    }
+  });
+
+  tagsBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const lastTagsElement = tagsArray[tagsArray.length - 1];
+
+    if (lastTagsElement.value !== "") {
+      const newTagInput = lastTagsElement.cloneNode(true);
+      newTagInput.value = "";
+      lastTagsElement.after(newTagInput);
+      tagsArray.push(newTagInput);
+    }
+  });
+
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+    const newMediaArray = [];
+    const newTagsArray = [];
+
+    mediaArray.forEach((input) => {
+      if (input.value !== "") {
+        newMediaArray.push(input.value);
+      }
+    });
+    tagsArray.forEach((input) => {
+      if (input.value !== "") {
+        newTagsArray.push(input.value);
+      }
+    });
+
     const data = {
       title: titleInput.value,
       description: descriptionInput.value,
-      tags: [tagsInput.value],
-      media: [mediaInput.value],
+      tags: newTagsArray,
+      media: newMediaArray,
     };
+
+    if (mediaInput.value === "") {
+      delete data.media;
+    }
 
     editListing(`${baseUrl}${specificListingUrl}`, data);
   });
